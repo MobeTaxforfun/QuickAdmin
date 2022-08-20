ae1af88bcf0c1717af7d7ac364eed72d20b93ec6
mobe <mobetax81072@gmail.com>
2022-08-20 23:14:07 +0800

    更新 FirstPage

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import "./home.scss";
+
+export const Home = () => {
+    return (
+        <div className="home">
+            <h1 className="title">Hello Mobe Wu</h1>
+        </div>
+    );
+};
+
+export default Home;
