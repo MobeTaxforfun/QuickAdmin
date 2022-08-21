0ddd77f9c9080cb93d788eeb44d673ebc4ef6035
mobe <mobetax81072@gmail.com>
2022-08-22 01:17:59 +0800

    更新 Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,55 @@
-import "./navbar.scss"
+import "./navbar.scss";
+import myLogo from '../../img/myavatar.jpg';
+
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeIcon className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineIcon className="icon" />
+            <div className="counter">6</div>
+          </div>
+          <div className="item">
+            <ListAltIcon className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src={myLogo}
+              alt=""
+              className="avatar">
+            </img>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
