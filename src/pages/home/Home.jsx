7de8ae6bbd891cce6311acc4e46a16c63e5c658c
mobe <mobetax81072@gmail.com>
2022-08-24 01:18:49 +0800

    更新 tablelist 相關

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Chart from "../../components/chart/Chart";
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import TableList from "../../components/table/Table";
 import Widget from "../../components/widget/Widget";
 
 import "./home.scss";
@@ -21,6 +22,10 @@ export const Home = () => {
                 <div className="charts">
                     <Featured />
                     <Chart />
+                </div>
+                <div className="listContainer">
+                    <div className="listTitle">SomeTable here</div>
+                    <TableList />
                 </div>
             </div>
         </div>
